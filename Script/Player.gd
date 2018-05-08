636245e127453006cf6508e5cb507059dd918a62
Dwi Wahyu <bxmailo@gmail.com>
2018-05-08 19:37:46 +0700

    May 8 init

--- Script/Player.gd
+++ Script/Player.gd
@@ -11,18 +11,13 @@ func _ready():
 	# Initialization here
 	print("Started")
 
-
-
 func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
-
 #cheat mode
 	if easterEgg>=5:
 		get_node("MainMenuContainer/btnSecret").text = "Cheat Mode"
-	
-	
 
 
 func _on_btnQuit_pressed():
@@ -36,15 +31,11 @@ func _on_btnPlay_pressed():
 func _on_btnSettings_pressed():
 	get_tree().change_scene("res://Scene/Settings.tscn")
 
-
-
 func _on_btnBack_pressed():
 	get_tree().change_scene("res://Scene/Mainmenu.tscn")
 
-
 func _on_btnExperimental_pressed():
 	easterEgg +=1
-
 
 func _on_btnSecret_pressed():
 	#get_tree().change_scene("res://Scene/World.tscn")
