4e70b622b10d3ae83c99063288149c8e0443c4f2
Dwi Wahyu <bxmailo@gmail.com>
2018-05-19 22:14:02 +0700

    farm Done

--- Script/PopUpFarm.gd
+++ Script/PopUpFarm.gd
@@ -1,6 +1,10 @@
 extends "res://Script/PlayContent.gd"
 
-var capacity = []
+var capacity = [100,400,700,999]
+var hargaChicken = 100
+var hargaCow = 200
+var hargaPakanChicken = 5
+var hargaPakanCow = 10
 
 func _ready():
 	pass
@@ -17,16 +21,32 @@ func _process(delta):
 	pass
 
 func _on_btnAddChicken_pressed():
+	if Global.Chicken < capacity[Global.lvlFarm-1] && Global.money >= hargaChicken :
+		Global.Chicken += 1
+		Global.money -= hargaChicken
+		get_node("/root/Play/BaseHud/StatusBar/" + "vboxDateMoney/lblMoney").text = "$ " + String(Global.money) 
 	pass # replace with function body
 
 
 func _on_btnAddCow_pressed():
+	if Global.Cow < capacity[Global.lvlFarm-1] && Global.money >= hargaCow :
+		Global.Cow += 1
+		Global.money -= hargaCow
+		get_node("/root/Play/BaseHud/StatusBar/" + "vboxDateMoney/lblMoney").text = "$ " + String(Global.money) 
 	pass # replace with function body
 
 
 func _on_btnAddPakanChicken_pressed():
+	if Global.pakanChicken < capacity[Global.lvlFarm-1] && Global.money >= hargaPakanChicken :
+		Global.pakanChicken += 1
+		Global.money -= hargaPakanChicken
+		get_node("/root/Play/BaseHud/StatusBar/" + "vboxDateMoney/lblMoney").text = "$ " + String(Global.money) 
 	pass # replace with function body
 
 
 func _on_lblAddPakanCow_pressed():
+	if Global.pakanCow < capacity[Global.lvlFarm-1] && Global.money >= hargaPakanCow :
+		Global.pakanCow += 1
+		Global.money -= hargaPakanCow
+		get_node("/root/Play/BaseHud/StatusBar/" + "vboxDateMoney/lblMoney").text = "$ " + String(Global.money) 
 	pass # replace with function body
